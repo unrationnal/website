3e7d3300c740c6a19d186ad03f06a9e42e68c535
Ousmane <kontaoousmane40@gmail.com>
2024-01-12 01:43:25 +0100

    view of home page

---
+++ src/components/main_page/home.tsx
@@ -0,0 +1,24 @@
+// Premier element de la page d'accueil
+
+import "./home.scss"
+
+import {type FC } from "react"
+
+export const Home:FC = () => {
+
+
+    return (
+        <>
+
+        <div className="msg_home">
+            <div>
+                <h1>Unrationnal</h1>
+                <p>Finding <span>rationnal</span> solutions for this world's problems</p>
+            </div>
+                
+        </div>
+        
+        </>
+    )
+}
+
