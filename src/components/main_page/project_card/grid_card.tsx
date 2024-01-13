5f458a1df5415520e5f05430af0ed148df80ac43
Bouscout <boudiallo1030@gmail.com>
2024-01-13 00:08:33 -0500

    added gridCard to index, improve and added blob

--- src/components/main_page/project_card/grid_card.tsx
+++ src/components/main_page/project_card/grid_card.tsx
@@ -9,17 +9,28 @@ const testSrc_1 = "https://images.unsplash.com/photo-1705002455875-29da8631d626?
 const testSrc_2 = "https://plus.unsplash.com/premium_photo-1666256629479-d049bc3cf40b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D"
 
 const testSrc_3 = "https://images.unsplash.com/photo-1703866772340-048b3877605d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D"
+
+const testSrc_4 = "https://images.unsplash.com/photo-1704895390342-b52a2f45786c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8fA%3D%3D"
+
+const testSrc_5 = "https://images.unsplash.com/photo-1704937197276-0094595fef4c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1OXx8fGVufDB8fHx8fA%3D%3D"
+
 export const GridCard:FC = () => {
+    const delay = 100
     return (
-        <section id="grid-card">
-            <Card image={testSrc} name="Vacation Expert" />
-            <Card description="Term and Services Specialist" />
-            <Card image={testSrc_3} name="Social Media Expert" description="Help you manage and develop your social media brand and reach more customers" />
-            <Card fontSize="0.6em" description='"Only those who will risk going too far can find out how possibly one can go"' italic={true}/>
-            <Card description="Master any subject with the Quiz Expert"/>
-            <Card name="LangFriend" description="Learn any new languague through friendly interactions" image={testSrc_1}/>
-            <Card name="Grocery" description="Manage your grocery list, your receipts and your shopping habit with the Grocery Expert" image={testSrc_2} />
-            <Card description='"The best way to predict the future is to create it"' italic={true}/>
+        <section id="catalogue">
+
+            <h2>Our Catalogue</h2>
+
+            <section id="grid-card">
+                <Card image={testSrc} name="Social Media Critic" delay={delay} />
+                <Card description="Term and Services Specialist" image={testSrc_5} delay={delay*2}/>
+                <Card image={testSrc_3} name="Social Media Expert" description="Help you manage and develop your social media brand and reach more customers" delay={delay*3} />
+                <Card fontSize="0.6em" description='"Only those who will risk going too far can find out how possibly one can go"' italic={true} delay={delay*4}/>
+                <Card description="Master any subject with the Quiz Expert" image={testSrc_4} delay={delay*5} />
+                <Card name="LangFriend" description="Learn any new languague through friendly interactions" image={testSrc_1} delay={delay*6}/>
+                <Card name="Grocery" description="Manage your grocery list, your receipts and your shopping habit with the Grocery Expert" image={testSrc_2} delay={delay*7}/>
+                <Card description='"The best way to predict the future is to create it"' italic={true} delay={delay*8} />
+            </section>
         </section>
     )
 }
@@ -31,12 +42,13 @@ interface CardProps {
     image? : string  ,
     fontSize? : string,
     italic? : boolean,
+    delay : number,
 }
 
-const Card:FC<CardProps> = ({image=null, name=null, description=null, fontSize=null, italic=false}) => {
+const Card:FC<CardProps> = ({delay, image=null, name=null, description=null, fontSize=null, italic=false}) => {
 
     const inStyle : React.CSSProperties = image ? {
-        backgroundImage : `url("${image}")`
+        backgroundImage : `linear-gradient(120deg, #090909 20%, transparent), linear-gradient(-10deg, #090909 20%, transparent), url("${image}")`,
     } : {}
 
     const h2Style : React.CSSProperties = fontSize ? {
@@ -46,13 +58,22 @@ const Card:FC<CardProps> = ({image=null, name=null, description=null, fontSize=n
     return (
         <div style={inStyle}>
             {name && 
-            <h2>{name}</h2>
+            <h2 style={{
+                animation : `intro-text 1000ms var(--slow-fast) ${delay}ms both`
+            }}>{name}</h2>
             }
 
             {description && 
             <h3 style={h2Style}>{
                 italic ? <i>{description}</i> : description
             }</h3>
+            }
+
+            {(image || name) &&
+            <button>
+                <h4>Explore</h4>
+                <i className="fa-solid fa-arrow-right"></i>
+            </button>
             }
         </div>
     )
