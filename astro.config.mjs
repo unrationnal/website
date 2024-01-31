7f2ec523264678d6d93f97c387673c1ec8e9ac43
Bouscout <boudiallo1030@gmail.com>
2024-01-30 23:24:51 -0500

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://unrationnal.github.io/website',
+  site: 'https://unrationnal.world',
 
   integrations: [react()]
 });
