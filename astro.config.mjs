efde1dfd2c8d8e8ec89e64784396eb2e0de36cd4
unrationnal <156369381+unrationnal@users.noreply.github.com>
2024-01-31 16:24:21 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+//site : "https://unrationnal.github.io/website/",
 export default defineConfig({
-  //site: 'https://unrationnal.world',
-  site : "https://unrationnal.github.io/website/",
+  site: 'https://unrationnal.world',
   integrations: [react()],
   output: "static",
 });
