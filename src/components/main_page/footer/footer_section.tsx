b084b0c7daf71f96be8befca67bdc83efcab372e
Bouscout <boudiallo1030@gmail.com>
2024-02-01 02:32:02 -0500

    footer link

--- src/components/main_page/footer/footer_section.tsx
+++ src/components/main_page/footer/footer_section.tsx
@@ -7,7 +7,7 @@ import "./footer.scss"
 const FooterSection:FC = () => {
     return (
         <footer>
-            <h2>Thanks for visiting, we are currently in development, for further details contact us at <span>unrationnal@gmail.com</span></h2>
+            <h2>Thanks for visiting, we are currently in development, for further details contact us at <span><a href="mailto:unrationnal@gmail.com">unrationnal@gmail.com</a></span></h2>
         </footer>
     )
 }
