7b2447136df060c11bf543780e5afff3e83095be
Bouscout <boudiallo1030@gmail.com>
2024-02-01 02:44:28 -0500

    env variable

--- src/components/firebase_configuration.ts
+++ src/components/firebase_configuration.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.PUBLIC_APIKEY,
-    authDomain: import.meta.env.PUBLIC_AUTHDOMAIN,
-    projectId: import.meta.env.PUBLIC_PROJECTID,
-    storageBucket: import.meta.env.PUBLIC_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.PUBLIC_MESSAGINGSENDERID,
-    appId: import.meta.env.PUBLIC_APPID,
-    measurementId: import.meta.env.PUBLIC_MEASUREMENTID,
+    apiKey: import.meta.env.PUBLIC_API_KEY,
+    authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
+    projectId: import.meta.env.PUBLIC_PROJECT_ID,
+    storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
+    appId: import.meta.env.PUBLIC_APP_ID,
+    measurementId: import.meta.env.PUBLIC_MEASUREMENT_ID,
 };
 
 console.log("the var : ", firebaseConfig)
