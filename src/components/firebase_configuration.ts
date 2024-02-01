25cb80eb91a15d4c32cb057ebefcb0f13a5aab86
Bouscout <boudiallo1030@gmail.com>
2024-02-01 03:55:04 -0500

    finalized firestore db

--- src/components/firebase_configuration.ts
+++ src/components/firebase_configuration.ts
@@ -13,9 +13,6 @@ const firebaseConfig = {
     measurementId: import.meta.env.PUBLIC_MEASUREMENT_ID,
 };
 
-console.log("the env var  is : ", import.meta.env)
-console.log("the var : ", firebaseConfig)
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
